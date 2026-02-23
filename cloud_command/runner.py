d3a94a36578fe44399cc11349a74bee9f579be01
androsh7@gmail.com <androsh7@gmail.com>
2026-02-22 20:54:20 -0500

    Added SSL support to runner

--- cloud_command/runner.py
+++ cloud_command/runner.py
@@ -2,6 +2,7 @@
 
 # Standard libraries
 import argparse
+from pathlib import Path
 
 # Third-party libraries
 import uvicorn
@@ -17,12 +18,14 @@ def main():
 
     # Add uvicorn arguments
     uvicorn_arguments = parser.add_argument_group("uvicorn options")
-    uvicorn_arguments.add_argument("--host", default="127.0.0.1", help="Uvicorn server IP, default: 127.0.0.1")
-    uvicorn_arguments.add_argument("--port", default=8080, help="Uvicorn server Port, default: 8080")
+    uvicorn_arguments.add_argument("--host", type=str, default="127.0.0.1", help="Uvicorn server IP, default: 127.0.0.1")
+    uvicorn_arguments.add_argument("--port", type=int, default=8080, help="Uvicorn server Port, default: 8080")
+    uvicorn_arguments.add_argument("--private-key-path", type=Path, default=None, help="Path to private key for SSH access to agents")
+    uvicorn_arguments.add_argument("--public-key-path", type=Path, default=None, help="Path to public key for SSH access to agents")
 
     args = parser.parse_args()
 
-    uvicorn.run(app=app, host=args.host, port=args.port)
+    uvicorn.run(app=app, host=args.host, port=args.port, private_key_path=args.private_key_path, public_key_path=args.public_key_path)
 
 
 if __name__ == "__main__":
