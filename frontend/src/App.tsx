905d67eeb10b020dd553dcabc2472227ab9476d9
androsh7@gmail.com <androsh7@gmail.com>
2026-03-15 12:42:06 -0400

    Added lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootswatch/dist/slate/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import ApiDocs from "./pages/ApiDocs";
-import InteractiveShell from "./pages/InteractiveShell";
+
+const Home = lazy(() => import("./pages/Home"));
+const InteractiveShell = lazy(() => import("./pages/InteractiveShell"));
+const ApiDocs = lazy(() => import("./pages/ApiDocs"));
 
 function NotFound() {
   return <div className="container mt-4">Page not found</div>;
@@ -19,6 +21,7 @@ export default function App() {
   return (
     <>
       <Navbar />
+      <Suspense fallback={<div className="container mt-4">Loading...</div>}>
       <Routes>
         <Route path="/" element={<Home />} errorElement={<InternalError />} />
         <Route
@@ -33,6 +36,7 @@ export default function App() {
           errorElement={<InternalError />}
         />
       </Routes>
+      </Suspense>
     </>
   );
 }
